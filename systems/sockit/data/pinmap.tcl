705c09fdfb06694e48798b72b8dfcc7791d3a8dc
Stefan Kristiansson <stefan.kristiansson@saunalahti.fi>
2013-09-11 08:19:32 +0300

    pinmap: make section comments "block comments"

--- systems/sockit/data/pinmap.tcl
+++ systems/sockit/data/pinmap.tcl
@@ -1,15 +1,21 @@
+#
 # Clock / Reset
+#
 set_location_assignment PIN_AD27 -to rst_n_pad_i
 set_location_assignment PIN_AF14 -to sys_clk_pad_i
 set_instance_assignment -name IO_STANDARD "1.5 V" -to sys_clk_pad_i
 
+#
 # UART
+#
 #set_location_assignment PIN_B25 -to uart0_srx_pad_i
 #set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_srx_pad_i
 #set_location_assignment PIN_C25 -to uart0_stx_pad_o
 #set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to uart0_stx_pad_o
 
+#
 # GPIO
+#
 # gpio0_io[3:0] connected to leds
 set_location_assignment PIN_AF10 -to gpio0_io[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to gpio0_io[0]
